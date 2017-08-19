cc31f2ad5e654c743db4f148ca27eaf0bf235b93
Fisab <vladsudakov5@gmail.com>
2017-08-19 16:56:48 +0300

    asdf

--- data_worker.py
+++ data_worker.py
@@ -5,11 +5,9 @@ def create_players(screen, pygame, size, map):
 	res = []
 
 	data = tools.load_file('config.json')
-#сделать генерацию позиции в Map
 	for i in data['players']:
 		if i['color'] == '':
 			i['color'] = '0,0,0'
-		#print(i['script_path'], i['data_file'])
 		res.append( player.Hero(screen, pygame, size, map, i['color'], i['nick'], i['script_path'], i['data_file']) )
 
 	return res
